102438ac77394c70bfcf98ae6bec796b9ec0ae6d
Prakhar <prakharnigam1997@gmail.com>
2022-01-19 23:45:45 +0530

    Beutify Profile page and add conditional routing

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -7,9 +7,10 @@ import { useContext } from "react";
 
 const useStyles = makeStyles({
     container: {
-        width : "60vw",
+        width : "50vw",
         height : "50vh",
-        margin : "50px auto"
+        margin : "50px auto",
+        backgroundColor : "#d6d8db"
     },
     innerContainer : {
         height : "100%",
@@ -54,23 +55,3 @@ export default function Profile() {
         </Card>
     )
 }
-
-{/* <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card> */}
